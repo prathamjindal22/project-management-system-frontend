63b6ad48ee3a5aef51f099007cbe64bb5c1b07d2
Pratham Jindal <jindalpratham22@gmail.com>
2026-03-18 15:20:56 +0530

    fixed an issue

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from '../pages/Login'
-import Projects from '../pages/Projects'
-import Taskboard from '../pages/TaskBoard'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Projects from "../pages/Projects";
+import Taskboard from "../pages/TaskBoard";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Login/>}/>
-            <Route path='/projects' element={<Projects/>}/>
-            <Route path='/project/:id' element={<Taskboard/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<Taskboard />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
